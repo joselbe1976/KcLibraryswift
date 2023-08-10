9e7c1bb4b8e3571d47116e1f6b74a64c7e5184a3
Jose Luis Bustos <jlbustos@equilibrha.es>
2023-08-10 12:43:03 +0200

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# KcLibraryswift
+
+A description of this package.
---
+++ Sources/KcLibraryswift/KcLibraryswift.swift
@@ -0,0 +1,6 @@
+public struct KcLibraryswift {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
